3a9fb09a287ab0adf7e7199f1e4d82758cadebef
Ashwini Kumar <37498182+Ash993@users.noreply.github.com>
2021-05-21 06:23:27 +0530

    Update announcements_page.dart

--- lib/Screens/User/announcements_page.dart
+++ lib/Screens/User/announcements_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Announcements extends StatelessWidget {
   @override
@@ -7,6 +9,78 @@ class Announcements extends StatelessWidget {
       appBar: AppBar(
         title: Text('Announcements'),
       ),
+      body: annouce(context),
     );
+  }
+
+  Future getAnnocement() async {
+    String link = "airbushack-default-rtdb.firebaseio.com";
+    var res = await http.get(Uri.https(link, "/announcs/.json"));
+    print("getted");
+    var data = [];
+    var temp = json.decode(res.body);
+    temp.forEach((k, v) => data.add([k, v["Heading"], v["body"]]));
+    return List.from(data.reversed);;
+  }
+
+  Widget annouce(context) {
+    return FutureBuilder(
+        future: getAnnocement(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              print('${snapshot.error} occured');
+              return Center(
+                child: Text(
+                  '${snapshot.error} occured',
+                  style: TextStyle(fontSize: 18),
+                ),
+              );
+              // if we got our data
+            } else if (snapshot.hasData) {
+              return Container(
+                  child: ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return listTile(snapshot, index, context);
+                      }));
+            }
+          }
+          return Center(child: CircularProgressIndicator());
+        });
+  }
+
+  Widget listTile(snapshot, index, context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(width: 0.5)),
+      ),
+      child: ListTile(
+        tileColor: Colors.white,
+        title: Text(snapshot.data[index][1]),
+        leading: Icon(Icons.notifications_active),
+        subtitle: snapshot.data[index][2].length<30?Text(snapshot.data[index][2]):Text(snapshot.data[index][2].substring(0, 30)+"...."),
+        onTap: () {
+          popupSheet1(context,snapshot.data[index][1],snapshot.data[index][2]);
+        },
+      ),
+    );
+  }
+
+  void popupSheet1(context,title,body) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: Text(title),
+          content: Text(body),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        ));
   }
 }
