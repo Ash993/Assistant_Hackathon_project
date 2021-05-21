bdee29cbe031ce8498cebbdf858df9681506e1a6
Ashwini Kumar <37498182+Ash993@users.noreply.github.com>
2021-05-21 15:51:17 +0530

    Update assistant_page.dart

--- lib/Screens/User/assistant_page.dart
+++ lib/Screens/User/assistant_page.dart
@@ -51,6 +51,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool clicked= false;
   int _page = 0;
 
   void _features(){
@@ -68,8 +69,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('User Assistance'),
 
+
       ),
       body: Scaffold(
+        body: assist(context),
 
 bottomNavigationBar: CupertinoTabBar(
 
@@ -77,17 +80,8 @@ bottomNavigationBar: CupertinoTabBar(
   backgroundColor: Colors.white,
   items: <BottomNavigationBarItem>[
     BottomNavigationBarItem(
-      icon: Icon(Icons.people_alt,
-        color: (_page == 0) ? Colors.black : Colors.grey,
-      ),
-      title: new Text('User Community', style: TextStyle(color: Colors.black87,
-          fontSize: 11.0,
-          fontWeight: FontWeight.bold),),
-      backgroundColor: Colors.white,
-    ),
-    BottomNavigationBarItem(
       icon: Icon(Icons.headset_mic,
-        color: (_page == 1) ? Colors.black : Colors.grey,
+        color: (_page == 0) ? Colors.black : Colors.grey,
 
       ),
       title: new Text('Chatbot', style: TextStyle(color: Colors.black87,
@@ -97,17 +91,17 @@ bottomNavigationBar: CupertinoTabBar(
     ),
     BottomNavigationBarItem(
       icon: Icon(Icons.bug_report_outlined,
-        color: (_page == 2) ? Colors.black : Colors.grey,
+        color: (_page == 1) ? Colors.black : Colors.grey,
 
       ),
-      title: new Text('Bug Reporting', style: TextStyle(color: Colors.black87,
+      title: new Text('Bug Reporting & Feedbacks', style: TextStyle(color: Colors.black87,
           fontSize: 11.0,
           fontWeight: FontWeight.bold),),
       backgroundColor: Colors.white,
     ),
     BottomNavigationBarItem(
       icon: Icon(Icons.announcement,
-        color: (_page == 3) ? Colors.black : Colors.grey,
+        color: (_page == 2) ? Colors.black : Colors.grey,
 
       ),
       title: new Text('Announcements', style: TextStyle(
@@ -116,56 +110,142 @@ bottomNavigationBar: CupertinoTabBar(
           fontWeight: FontWeight.bold),),
       backgroundColor: Colors.white,
     ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.dynamic_feed,
-        color: (_page == 4) ? Colors.black : Colors.grey,
-
-      ),
-      title: new Text('Live Feedback', style: TextStyle(color: Colors.black87,
-          fontSize: 11.0,
-          fontWeight: FontWeight.bold),),
-      backgroundColor: Colors.white,
-    ),
+    // BottomNavigationBarItem(
+    //   icon: Icon(Icons.dynamic_feed,
+    //     color: (_page == 4) ? Colors.black : Colors.grey,
+    //
+    //   ),
+    //   title: new Text('Live Feedback', style: TextStyle(color: Colors.black87,
+    //       fontSize: 11.0,
+    //       fontWeight: FontWeight.bold),),
+    //   backgroundColor: Colors.white,
+    // ),
 
   ],
   onTap: (index) {
     setState(() {
       _page = index;
     });
+
     if (_page == 0) {
       return Navigator.push(context,
-        MaterialPageRoute(builder: (context) => UserCommunity()),
+        MaterialPageRoute(builder: (context) => ChatDetailPage()),
       );
     }
     if (_page == 1) {
-      return Navigator.push(context,
-        MaterialPageRoute(builder: (context) => ChatBot()),
-      );
+      _features();
     }
     if (_page == 2) {
-      return Navigator.push(context,
-        MaterialPageRoute(builder: (context) => BugReporting()),
-      );
-    }
-    if (_page == 3) {
       return Navigator.push(context,
         MaterialPageRoute(builder: (context) => Announcements()),
       );
     }
-    if (_page == 4) {
-      return Navigator.push(context,
-        MaterialPageRoute(builder: (context) => LiveFeedback()),
-      );
-    }
+    // if (_page == 4) {
+    //   return Navigator.push(context,
+    //     MaterialPageRoute(builder: (context) => LiveFeedback()),
+    //   );
+    // }
   },
 
 ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: _features,
-        ),
+        // floatingActionButton: Padding(
+        //   padding: const EdgeInsets.only(right: 275.0),
+        //   child: FloatingActionButton(
+        //     child: Icon(Icons.add),
+        //     onPressed: _features,
+        //   ),
+        // ),
       ),
     );
 
   }
+
+
+  Widget assist(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+            bottom: 150,
+            right: 5,
+            child: RawMaterialButton(
+              elevation: 2.0,
+              padding: EdgeInsets.all(15.0),
+              shape: CircleBorder(),
+              fillColor: Colors.purple,
+              child: Icon(Icons.assistant_rounded,size: 25,color: Colors.white,),
+              onPressed: () {
+                setState(() {
+                  if (this.clicked)
+                    this.clicked=false;
+                  else
+                    this.clicked=true;
+                });
+              },
+            )),
+        this.clicked?Positioned(
+            bottom: 240,
+            right: 0,
+            child: RawMaterialButton(
+              elevation: 2.0,
+              padding: EdgeInsets.all(15.0),
+              shape: CircleBorder(),
+              fillColor: Colors.blue,
+              child: Icon(Icons.people_alt,size: 25,color: Colors.white,),
+              onPressed: () {
+              },
+            )):Container(),
+        this.clicked?Positioned(
+            bottom: 60,
+            right: 0,
+            child: RawMaterialButton(
+              elevation: 2.0,
+              padding: EdgeInsets.all(15.0),
+              shape: CircleBorder(),
+              fillColor: Colors.blue,
+              child: Icon(Icons.dynamic_feed,size: 25,color: Colors.white,),
+              onPressed: () {
+              },
+            )):Container(),
+        this.clicked?Positioned(
+            bottom: 210,
+            right: 60,
+            child: RawMaterialButton(
+              elevation: 2.0,
+              padding: EdgeInsets.all(15.0),
+              shape: CircleBorder(),
+              fillColor: Colors.blue,
+              child: Icon(Icons.headset_mic,size: 25,color: Colors.white,),
+              onPressed: () {
+              },
+            )):Container(),
+        this.clicked?Positioned(
+            bottom: 90,
+            right: 60,
+            child: RawMaterialButton(
+              elevation: 2.0,
+              padding: EdgeInsets.all(15.0),
+              shape: CircleBorder(),
+              fillColor: Colors.blue,
+              child: Icon(Icons.announcement,size: 25,color: Colors.white,),
+              onPressed: () {
+              },
+            )):Container(),
+        this.clicked?Positioned(
+            bottom: 150,
+            right: 80,
+            child: RawMaterialButton(
+              elevation: 2.0,
+              padding: EdgeInsets.all(15.0),
+              shape: CircleBorder(),
+              fillColor: Colors.blue,
+              child: Icon(Icons.bug_report_outlined,size: 25,color: Colors.white,),
+              onPressed: () {
+              },
+            )):Container(),
+      ],
+    );
+  }
 }
+
+
+
