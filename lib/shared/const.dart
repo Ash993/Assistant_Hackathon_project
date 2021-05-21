f099b6d7c36d20722108cb28d62240c873e1081e
Ashwini Kumar <37498182+Ash993@users.noreply.github.com>
2021-05-21 10:15:30 +0530

    Update const.dart

--- lib/shared/const.dart
+++ lib/shared/const.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+String projectID="airbus-assistant-zthqbed";
+String secretKey ="x1rjbx0jirjmb2vlcodi7j9hha7dq7d2hn885p6a51lpcd2s";
