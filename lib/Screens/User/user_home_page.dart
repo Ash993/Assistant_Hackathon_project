08e586811dbc5e3ea39f35b19511d0c146969644
Ashwini Kumar <37498182+Ash993@users.noreply.github.com>
2021-05-21 20:30:46 +0530

    Update user_home_page.dart

--- lib/Screens/User/user_home_page.dart
+++ lib/Screens/User/user_home_page.dart
@@ -1,7 +1,24 @@
+import 'package:alan_voice/alan_voice.dart';
 import 'package:flutter/material.dart';
 import 'package:helpingwidget/Screens/User/assistant_page.dart';
 
-class UserHomePage extends StatelessWidget {
+class UserHomePage extends StatefulWidget {
+  @override
+  _UserHomePageState createState() => _UserHomePageState();
+}
+
+class _UserHomePageState extends State<UserHomePage> {
+  void initState(){
+    super.initState();
+    setupAlan();
+  }
+  setupAlan() {
+    AlanVoice.addButton("4ccdd3fef76614d96e1cc06d3a33e1262e956eca572e1d8b807a3e2338fdd0dc/stage",
+        buttonAlign: AlanVoice.BUTTON_ALIGN_RIGHT
+    );
+    // AlanVoice.callbacks.add((command) => _handleCommand(command.data));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
