615cdab22cf6c56b69916695b4c6f240695c603f
Ashwini Kumar <37498182+Ash993@users.noreply.github.com>
2021-05-21 15:49:44 +0530

    Update techteam_page.dart

--- lib/Screens/Admin/techteam_page.dart
+++ lib/Screens/Admin/techteam_page.dart
@@ -4,6 +4,8 @@ import 'package:helpingwidget/Screens/Admin/announcing_page.dart';
 import 'package:helpingwidget/Screens/Admin/bugreports_page.dart';
 import 'package:helpingwidget/Screens/Admin/involve_page.dart';
 import 'package:helpingwidget/Screens/Admin/livefeedback_page.dart';
+import 'package:simple_url_preview/simple_url_preview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 class TechTeam extends StatefulWidget {
@@ -12,6 +14,24 @@ class TechTeam extends StatefulWidget {
 }
 
 class _TechTeamState extends State<TechTeam> {
+
+  openURL() async{
+    if(await canLaunch("https://console.wiredash.io/project/airbus-assistant-zthqbed/feedback/60a66895828dca000e59d32a")){
+      await launch("https://console.wiredash.io/project/airbus-assistant-zthqbed/feedback/60a66895828dca000e59d32a");
+
+    }else{
+      throw 'Could Not Launch URL';
+    }
+  }
+  openURL2() async{
+    if(await canLaunch("https://console.wiredash.io/project/airbus-assistant-zthqbed/feedback/60a668ad828dca000e59d32b")){
+      await launch("https://console.wiredash.io/project/airbus-assistant-zthqbed/feedback/60a668ad828dca000e59d32b");
+
+    }else{
+      throw 'Could Not Launch URL';
+    }
+  }
+
   @override
   int _page=0;
   Widget build(BuildContext context) {
@@ -20,6 +40,7 @@ class _TechTeamState extends State<TechTeam> {
         title: Text('Technical Team'),
       ),
       body: Scaffold(
+
 
         bottomNavigationBar: CupertinoTabBar(
 
@@ -55,17 +76,17 @@ class _TechTeamState extends State<TechTeam> {
                   fontWeight: FontWeight.bold),),
               backgroundColor: Colors.white,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat_rounded,
-                color: (_page == 3) ? Colors.black : Colors.grey,
-
-              ),
-              title: new Text('Involve', style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 11.0,
-                  fontWeight: FontWeight.bold),),
-              backgroundColor: Colors.white,
-            ),
+            // BottomNavigationBarItem(
+            //   icon: Icon(Icons.chat_rounded,
+            //     color: (_page == 3) ? Colors.black : Colors.grey,
+            //
+            //   ),
+            //   title: new Text('Involve', style: TextStyle(
+            //       color: Colors.black87,
+            //       fontSize: 11.0,
+            //       fontWeight: FontWeight.bold),),
+            //   backgroundColor: Colors.white,
+            // ),
 
           ],
           onTap: (index) {
@@ -78,20 +99,16 @@ class _TechTeamState extends State<TechTeam> {
               );
             }
             if (_page == 1) {
-              return Navigator.push(context,
-                MaterialPageRoute(builder: (context) => BugReports()),
-              );
+              openURL();
             }
             if (_page == 2) {
-              return Navigator.push(context,
-                MaterialPageRoute(builder: (context) => LiveFeedbacks()),
-              );
+              openURL2();
             }
-            if (_page == 3) {
-              return Navigator.push(context,
-                MaterialPageRoute(builder: (context) => Involve()),
-              );
-            }
+            // if (_page == 3) {
+            //   return Navigator.push(context,
+            //     MaterialPageRoute(builder: (context) => Involve()),
+            //   );
+            // }
           },
 
         ),
